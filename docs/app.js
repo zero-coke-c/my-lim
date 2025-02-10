e5d7390658be1b2fe90574b4c71217964f0e6d5f
lililmilm <jeongu8807@daum.net>
2025-02-10 14:25:29 +0900

    json

--- docs/app.js
+++ docs/app.js
@@ -1,11 +1,25 @@
 async function main() {
-  async function hanldeCC() {
-    document.querySelector("#box").textContent = await (
-      await fetch("http://127.0.0.1:3000")
-    ).text();
-  }
+  async function hanldeCC(event) {
+    event.preventDefault();
+    const url = "http://127.0.0.1:3000";
 
-  document.querySelector("#ccBtn").addEventListener("click", hanldeCC);
+    const formData = new FormData((form = document.querySelector("#ccForm")));
+
+    const text = formData.get("text");
+    console.log(text);
+    // header content 꼭넣기
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ text }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+    document.querySelector("#box").textContent = JSON.stringify(json);
+  }
+  //   document.querySelector("#ccBtn").addEventListener("click", hanldeCC);
+  document.querySelector("#ccForm").addEventListener("submit", hanldeCC);
 }
 
 document.addEventListener("DOMContentLoaded", main);
