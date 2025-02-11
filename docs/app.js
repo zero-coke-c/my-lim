f04cd50d1bca8fa43e2e7e59f700c2d6626e6440
lililmilm <jeongu8807@daum.net>
2025-02-11 22:25:08 +0900

    merge

--- docs/app.js
+++ docs/app.js
@@ -1,12 +1,19 @@
 async function main() {
   async function hanldeCC(event) {
     event.preventDefault();
+
+    // loading...
+    const spinner = document.createElement("div");
+    spinner.classList.add("spinner-border");
+    document.querySelector("#box").appendChild(spinner);
+
     const url = "http://127.0.0.1:3000";
+    // const url = "https://gossamer-mercury-wildebeest.glitch.me";
 
     const formData = new FormData((form = document.querySelector("#ccForm")));
 
     const text = formData.get("text");
-    console.log(text);
+
     // header content 꼭넣기
     const response = await fetch(url, {
       method: "POST",
@@ -15,8 +22,23 @@ async function main() {
         "Content-Type": "application/json",
       },
     });
+    spinner.remove();
+
     const json = await response.json();
-    document.querySelector("#box").textContent = JSON.stringify(json);
+
+    const { image, desc } = json;
+
+    const box = document.querySelector("#box");
+    box.innerHTML = "";
+    const imageTag = document.createElement("img");
+    imageTag.classList.add("img-fluid");
+    imageTag.src = image; // image - link
+    const descTag = document.createElement("p");
+    descTag.textContent = desc;
+    box.appendChild(imageTag);
+    box.appendChild(descTag);
+
+    // document.querySelector("#box").textContent = JSON.stringify(json);
   }
   //   document.querySelector("#ccBtn").addEventListener("click", hanldeCC);
   document.querySelector("#ccForm").addEventListener("submit", hanldeCC);
